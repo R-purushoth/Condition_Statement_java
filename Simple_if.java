19904c78000a3c43041e496fa482196a2f9acd15
shothp345@gmail.com <shothp345@gmail.com>
2025-07-11 09:38:52 +0530

    first commit

---
+++ Simple_if.java
@@ -0,0 +1,9 @@
+public class Simple_if{
+    public static void main(String args[]){
+        // This program checks if a person is eligible to vote based on their age
+        int a=23; // Example age Value of a is 23
+        if (a > 18){
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+} 
