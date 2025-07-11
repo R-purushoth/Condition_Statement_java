bb9d3ab53062ce589007e028a5d5a72078071c97
shothp345@gmail.com <shothp345@gmail.com>
2025-07-11 09:47:42 +0530

    Simple_if.java

--- Simple_if.java
+++ Simple_if.java
@@ -1,6 +1,7 @@
 public class Simple_if{
     public static void main(String args[]){
         // This program checks if a person is eligible to vote based on their age
+        // input are initialized directly in the code not form the user
         int a=23; // Example age Value of a is 23
         if (a > 18){
             System.out.println("You are not eligible to vote.");
